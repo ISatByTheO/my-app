dfa724927dc668c49d325835449638710e539904
ISatByTheO <anthonyffernandes@gmail.com>
2020-08-06 09:04:13 -0400

    Updates

--- precache-manifest.48342bb2d7d5b27514086cd5508bd26d.js
+++ precache-manifest.48342bb2d7d5b27514086cd5508bd26d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3fce58cddcd90f75e401d24312b94656",
+    "revision": "f7f568a96d50a619482e801b31804678",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "63302c5d1bd2605c5be4",
+    "revision": "2a4a9117f7198057d1b9",
     "url": "/portfolio/static/css/main.e8986148.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.8f18329a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "63302c5d1bd2605c5be4",
-    "url": "/portfolio/static/js/main.830bf123.chunk.js"
+    "revision": "2a4a9117f7198057d1b9",
+    "url": "/portfolio/static/js/main.fbd275b6.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6a10a4669f3106ffcd2f8da7dd42b074.js"
+  "/portfolio/precache-manifest.48342bb2d7d5b27514086cd5508bd26d.js"
 );
 
 self.addEventListener('message', (event) => {
